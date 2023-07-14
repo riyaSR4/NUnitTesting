186e5cd04f3548db3b99c4504253b1d7103b9184
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-14 23:12:41 +0530

    [Riya] Add : Implemented Day of Week

---
+++ NUnitTesting/DayOfWeek.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTesting
+{
+    public class DayOfWeek
+    {
+        public void PrintDayOfWeek()
+        {
+            Console.WriteLine(" Enter the day : ");
+            int date = Convert.ToInt32(Console.ReadLine());
+            Console.Write(" Enter the month : ");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.Write(" Enter the year : ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            DateTime ss = new DateTime(date, month, year);
+
+            Console.WriteLine(ss.DayOfWeek);
+        }
+    }
+}
--- NUnitTesting/Program.cs
+++ NUnitTesting/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+
+namespace NUnitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter input to be executed\n 1.Day of Week\n 2.Exit\n");
+                int input = Convert.ToInt32(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        DayOfWeek dayOfWeek = new DayOfWeek();
+                        dayOfWeek.PrintDayOfWeek();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+
+                }
+            }
+        }
+    }
+}
